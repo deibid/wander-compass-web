10f5ae5886cd945237953ba65e87539f9210efca
deibid <davidazar3@gmail.com>
2019-05-05 01:20:11 -0400

    patching ports for heroku

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ io.on('connection', (socket) => {
 
 });
 
-http.listen(process.env.port || 3000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port 3000...");
 })
 
